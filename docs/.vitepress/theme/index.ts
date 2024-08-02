d0e7918b82ea2efe7d192e8955bbafa0a8122938
dpoqb <2980832735@qq.com>
2024-08-03 00:10:56 +0800

    update the structure of files

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import DefaultTheme from 'vitepress/theme';
 import Layout from './Layout.vue';
+import l2d_models from "../../public/l2d_models";
 
 export default {
   ...DefaultTheme,
@@ -8,51 +9,7 @@ export default {
     if (!import.meta.env.SSR) {
       const { loadOml2d } = await import('oh-my-live2d');
       loadOml2d({
-        models: [
-          {
-            "path": "https://model.oml2d.com/Pio/model.json",
-            "scale": 0.4,
-            "position": [0, 50],
-            "stageStyle": {
-            "height": 300
-            },
-          },
-          {
-            "path": "https://model.oml2d.com/chino/model.json",
-            "scale": 0.2,
-            "position": [0, 50],
-            "stageStyle": {
-              "height": 300
-            }
-          },
-          {
-            "path": "/l2d_models/hiyori_free_zh/runtime/hiyori_free_t08.model3.json",
-            "scale": 0.15,
-            "position": [-85, 0],
-            "stageStyle": {
-              "height": 300,
-              "width": 270,
-            }
-          },
-          {
-            "path": "/l2d_models/ena_jc/19ena_jc.model3.json",
-            "scale": 0.175,
-            "position": [-100, 0],
-            "stageStyle": {
-              "height": 350,
-              "width": 300,
-            }
-          },
-          {
-            "path": "/l2d_models/miku_normal/21miku_normal.model3.json",
-            "scale": 0.175,
-            "position": [-100, 0],
-            "stageStyle": {
-              "height": 350,
-              "width": 300,
-            }
-          }
-        ]
+        models: l2d_models
       });
     }
   }
